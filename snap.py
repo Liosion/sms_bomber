04e339ca6468011b7c342c13be6f0bcc4b21dd2f
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 04:50:12 +0430

    Update snap.py

--- snap.py
+++ snap.py
@@ -4,13 +4,16 @@ import time
 from threading import Thread
 
 phone = input("Set Your Number Via +98==>\n\r")
-snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
-snapD = {"cellphone":phone}
-snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
+def snap(phone):
+    snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
+    snapD = {"cellphone":phone}
+    snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
+    snapp = snapR.text
+    if "OK" in snapp:
+        print("Sended")
+    else:
+        print("Fuck!")
 
-snap = snapR.text
-
-if "OK" in snap:
-  print("Sended")
-else:
-  print("Fuck!")
+def main(phone):
+  Thread(target=snap, args=[phone]).start()
+  time.sleep(3)

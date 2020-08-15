c1ef83f83b563250f9d29856a10a5c4cd951ae9b
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 04:41:21 +0430

    Update snap.py

--- snap.py
+++ snap.py
@@ -1,15 +1,16 @@
 import requests
 import os
 import time
+from threading import Thread
 
 phone = input("Set Your Number Via +98==>\n\r")
 snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
 snapD = {"cellphone":phone}
-    try:
-        snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
-        if "OK" in snapR.text:
-            print ("sended sms:)")
-        else:
-            print ("Error!")
-    except:
-        print ("Error!")
+snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
+
+snap = snapR.text
+
+if "OK" in snap:
+  print("Sended")
+else:
+  print("Fuck!")

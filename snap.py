6426a54d9a86e54ced9fdc4a73c1e1a8ee977faa
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 05:06:09 +0430

    Update snap.py

--- snap.py
+++ snap.py
@@ -1,12 +1,10 @@
 import requests
 import os
 import time
-from requests.exceptions import HTTPError
 
 os.system('cls' if os.name=='nt' else 'clear')
 phone = input("Set Your Number Via +98==>\n\r")
 def snap(phone):
-  try:
     snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
     snapD = {"cellphone":phone}
     snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
@@ -15,6 +13,3 @@ def snap(phone):
         print("Sended")
     else:
         print("Fuck!")
-   except HTTPError:
-    time.sleep(5)
- return snapR

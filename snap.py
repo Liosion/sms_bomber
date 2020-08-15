fbd0e2fc59a6775707de1c857dc1fc8b958462b3
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 04:33:35 +0430

    Update snap.py

--- snap.py
+++ snap.py
@@ -1,6 +1,8 @@
 import requests
+import os
+import time
 
-phone = str(input("Set Your Number Via +98==>\n\r"))
+phone = input("Set Your Number Via +98==>\n\r")
 snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
 snapD = {"cellphone":phone}
     try:
@@ -10,4 +12,4 @@ snapD = {"cellphone":phone}
         else:
             print ("Error!")
     except:
-	    print ("Error!")
+        print ("Error!")

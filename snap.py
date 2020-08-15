5d0cc32a66d6312ae4bb1ac7c04523bb11f08971
Liosion Mikealson <57588337+Liosion@users.noreply.github.com>
2020-08-15 05:09:23 +0430

    Update snap.py

--- snap.py
+++ snap.py
@@ -4,11 +4,10 @@ import time
 
 os.system('cls' if os.name=='nt' else 'clear')
 phone = input("Set Your Number Via +98==>\n\r")
-def snap(phone):
-    snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
-    snapD = {"cellphone":phone}
-    snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
-    snapp = snapR.text
+snapH = {"x-app-name":"passenger-android", "Package-Name":"cab.snapp.passenger.play", "locale":"fa-IR", "x-app-version-code":"207", "x-app-version":"207", "Content-Type":"application/json; charset=utf-8", "Host":"oauth-passenger.snapp.site", "Accept-Encoding":"gzip, deflate"}
+snapD = {"cellphone":phone}
+snapR = requests.post("https://oauth-passenger.snapp.site/v2/otp", headers=snapH, json=snapD)
+snapp = snapR.text
     if "OK" in snapp:
         print("Sended")
     else:
